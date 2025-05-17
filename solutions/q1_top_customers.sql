4dfbf255aff6b675c78a9f6bc53884de66513812
Tarafder Tarikul Islam <114969575+Tarikul0001@users.noreply.github.com>
2025-05-18 05:11:50 +0600

    Update q1_top_customers.sql

--- solutions/q1_top_customers.sql
+++ solutions/q1_top_customers.sql
@@ -1,4 +1,6 @@
 --Q.1 Write a query to identify the top 10 over-70-year-old clients with the biggest internet sales. Name, age, phone number, email address, and total sales amount of the consumer are displayed.
+
+
 SELECT TOP 10 C.FirstName + ' ' + C.LastName AS Customer_Name, C.AddressLine1, DATEDIFF(YEAR,CONVERT(DATE,BirthDate),GETDATE()) AS Age, C.Phone, C.EmailAddress
 , ROUND (SUM(FIS.SalesAmount),2) AS Total_Sales
 FROM dbo.DimCustomer C
